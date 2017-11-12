eef789830211400bae6b4afe0af660bac62fcb35
Ivan Opanasiuk <vanjoker88@gmail.com>
2017-11-12 10:58:23 +0200

    home1

--- home-1/src/setupTests.js
+++ home-1/src/setupTests.js
@@ -1,14 +1,16 @@
-import {onfigure} from 'enzyme';
+import {configure} from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 
 configure({adapter: new Adapter()});
 
-class App extends Component {
-    state = {}
-    
-    render() { 
-        return ( <div>HEllo</div> )
-    }
-}
+let App = (prop) => (
+    <div className="App">
+        <footer>
+            <p>
+                Loftschool
+            </p>
+        </footer>
+    </div>
+)
  
 export default App;
